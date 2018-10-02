d679bc7ed11262d093469acd36c46d36db23ccd3
PC-201810011809\Administrator <827981516>
2018-10-02 19:22:49 +0800

    提交新内容

---
+++ 账号1的cookies.js
@@ -0,0 +1,55 @@
+// 账号2166187371手机号15828115841验证码"704799"的登录cookies
+
+// cookie:perf_ssid=f1iw613bi7gtbyot3en79s5nhi51vcn0_2018-10-02;
+// ubt_ssid=hi6sjatq5c2ybs2tzy0654f3s624holw_2018-10-02;
+// _utrace=a0ae29c85e808d43fa1aeb00ab39882c_2018-10-02;
+// snsInfo[101204453]=%7B%22city%22%3A%22%E8%AE%B8%E6%98%8C%22%2C%22constellation%22%3A%22%22%2C%22eleme_key%22%3A%22d6afd7bf03c02d1edaa06e94931d6810%22%2C%22figureurl%22%3A%22http%3A%2F%2Fqzapp.qlogo.cn%2Fqzapp%2F101204453%2F0F260E637411A5875F5F752C355CADE6%2F30%22%2C%22figureurl_1%22%3A%22http%3A%2F%2Fqzapp.qlogo.cn%2Fqzapp%2F101204453%2F0F260E637411A5875F5F752C355CADE6%2F50%22%2C%22figureurl_2%22%3A%22http%3A%2F%2Fqzapp.qlogo.cn%2Fqzapp%2F101204453%2F0F260E637411A5875F5F752C355CADE6%2F100%22%2C%22figureurl_qq_1%22%3A%22http%3A%2F%2Fthirdqq.qlogo.cn%2Fqqapp%2F101204453%2F0F260E637411A5875F5F752C355CADE6%2F40%22%2C%22figureurl_qq_2%22%3A%22http%3A%2F%2Fthirdqq.qlogo.cn%2Fqqapp%2F101204453%2F0F260E637411A5875F5F752C355CADE6%2F100%22%2C%22gender%22%3A%22%E7%94%B7%22%2C%22is_lost%22%3A0%2C%22is_yellow_vip%22%3A%220%22%2C%22is_yellow_year_vip%22%3A%220%22%2C%22level%22%3A%220%22%2C%22msg%22%3A%22%22%2C%22nickname%22%3A%22%E8%90%BD%E9%9B%AA%E5%80%BE%E5%9F%8E1%22%2C%22openid%22%3A%220F260E637411A5875F5F752C355CADE6%22%2C%22province%22%3A%22%E6%B2%B3%E5%8D%97%22%2C%22ret%22%3A0%2C%22vip%22%3A%220%22%2C%22year%22%3A%222000%22%2C%22yellow_vip_level%22%3A%220%22%2C%22name%22%3A%22%E8%90%BD%E9%9B%AA%E5%80%BE%E5%9F%8E1%22%2C%22avatar%22%3A%22http%3A%2F%2Fthirdqq.qlogo.cn%2Fqqapp%2F101204453%2F0F260E637411A5875F5F752C355CADE6%2F40%22%7D;
+// track_id=1538464127|10379ea2f6ab37d7d782db0b42f1a8193a3afdb75e67e5f86b|4f656a28fbbfeb068c332cec723404ca;
+// USERID=2071801298;
+// SID=E83YhMAdj5DnQcbxY9ITzvHERWsbIqUPPFkA
+
+
+// 账号 3470281466 手机号 17072314451 验证码"110875"的登录cookies
+
+// perf_ssid=jhlh50zlqw1ttksnvie4cfumb0platfh_2018-10-02;
+// ubt_ssid=cuby72r0ryaon5wiwg9b6ftsy34at5re_2018-10-02;
+// _utrace=de6ae5fa73435e1a8390e9dd1b74d4b3_2018-10-02;1538467778|602988a8c3e72adaa20
+// snsInfo[101204453]=%7B%22city%22%3A%22%E8%AE%B8%E6%98%8C%22%2C%22constellation%22%3A%22%22%2C%22eleme_key%22%3A%2263b103392177b54df2d24880bbe2021c%22%2C%22figureurl%22%3A%22http%3A%2F%2Fqzapp.qlogo.cn%2Fqzapp%2F101204453%2FBB3CBCED23B0DE540CDE12B420F2BD27%2F30%22%2C%22figureurl_1%22%3A%22http%3A%2F%2Fqzapp.qlogo.cn%2Fqzapp%2F101204453%2FBB3CBCED23B0DE540CDE12B420F2BD27%2F50%22%2C%22figureurl_2%22%3A%22http%3A%2F%2Fqzapp.qlogo.cn%2Fqzapp%2F101204453%2FBB3CBCED23B0DE540CDE12B420F2BD27%2F100%22%2C%22figureurl_qq_1%22%3A%22http%3A%2F%2Fthirdqq.qlogo.cn%2Fqqapp%2F101204453%2FBB3CBCED23B0DE540CDE12B420F2BD27%2F40%22%2C%22figureurl_qq_2%22%3A%22http%3A%2F%2Fthirdqq.qlogo.cn%2Fqqapp%2F101204453%2FBB3CBCED23B0DE540CDE12B420F2BD27%2F100%22%2C%22gender%22%3A%22%E7%94%B7%22%2C%22is_lost%22%3A0%2C%22is_yellow_vip%22%3A%220%22%2C%22is_yellow_year_vip%22%3A%220%22%2C%22level%22%3A%220%22%2C%22msg%22%3A%22%22%2C%22nickname%22%3A%22%E8%90%BD%E9%9B%AA%E5%80%BE%E5%9F%8E2%22%2C%22openid%22%3A%22BB3CBCED23B0DE540CDE12B420F2BD27%22%2C%22province%22%3A%22%E6%B2%B3%E5%8D%97%22%2C%22ret%22%3A0%2C%22vip%22%3A%220%22%2C%22year%22%3A%222000%22%2C%22yellow_vip_level%22%3A%220%22%2C%22name%22%3A%22%E8%90%BD%E9%9B%AA%E5%80%BE%E5%9F%8E2%22%2C%22avatar%22%3A%22http%3A%2F%2Fthirdqq.qlogo.cn%2Fqqapp%2F101204453%2FBB3CBCED23B0DE540CDE12B420F2BD27%2F40%22%7D;
+// track_id=e1f488d49fb4d94ce9dd07ce1d5bcaf|63f84ddc6ccda832f25efa06445ea831;
+// USERID=175546103;
+// SID=dPjR2OJ8V6acuvu5aZEd0rdHM3llV1f3thTA
+
+
+//其实抢红包实际提交的接口就是这个
+//https://h5.ele.me/restapi/marketing/promotion/weixin/0F260E637411A5875F5F752C355CADE6
+//提交的主要信息以及上面的cookies
+//{"method":"phone","group_sn":"2a0d0e4fd8ad50de","sign":"d6afd7bf03c02d1edaa06e94931d6810","phone":"","device_id":"","hardware_id":"","platform":4,"track_id":"1538464127|10379ea2f6ab37d7d782db0b42f1a8193a3afdb75e67e5f86b|4f656a28fbbfeb068c332cec723404ca","weixin_avatar":"http://thirdqq.qlogo.cn/qqapp/101204453/0F260E637411A5875F5F752C355CADE6/40","weixin_username":"落雪倾城1","unionid":"fuck","latitude":"","longitude":""}
+//分享出去的红包url链接
+//https://h5.ele.me/hongbao/#hardware_id=&is_lucky_group=True&lucky_number=0&track_id=&platform=4&sn=2a0d0e4fd8ad50de&theme_id=3177&device_id=&refer_user_id=470512338
+//1538464127|10379ea2f6ab37d7d782db0b42f1a8193a3afdb75e67e5f86b|4f656a28fbbfeb068c332cec723404ca
+//"2a0d0e4fd8ad50de"
+//0F260E637411A5875F5F752C355CADE6是qq图片url里面的内容openid
+//0F260E637411A5875F5F752C355CADE6
+//group_sn参数是分享出的红包里面的携带参数
+//"d6afd7bf03c02d1edaa06e94931d6810"
+
+//整合
+//大致明白了饿了么是怎么一个抢红包机制了有意思用的是拼接的方式
+//url里面的参数是cookies里面的snsInfo[101204453]的openid
+//提交参数里面的group_sn是红包链接里面的sn
+//sign是cookies里面的snsInfo[101204453]的eleme_key
+//track_id就是把cookies里面的track_id直接提交上去
+//unionid就fuck
+//weixin_avatar就是用户的三方平台的头像
+//weixin_username是第三方平台的用户名
+
+// https://h5.ele.me/hongbao/#hardware_id=
+// &is_lucky_group=True
+// &lucky_number=0
+// &track_id=
+// &platform=4
+// &sn=2a0d0e4fd8ad50de
+// &theme_id=3177
+// &device_id=
+// &refer_user_id=470512338
+// &code=58ACA985BC79FC796B92E25A123B2AF3
